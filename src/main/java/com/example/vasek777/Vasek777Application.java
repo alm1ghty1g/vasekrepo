81bf81d26feb345d3cce5e8469ef15d93ac70b83
vasek380 <vasek380@gmail.com>
2022-08-08 01:05:55 -0400

    commit

---
+++ src/main/java/com/example/vasek777/Vasek777Application.java
@@ -0,0 +1,13 @@
+package com.example.vasek777;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vasek777Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Vasek777Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vasek777/Vasek777ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vasek777;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vasek777ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
